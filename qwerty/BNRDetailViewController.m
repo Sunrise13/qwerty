93a25bb56ca2026e0b46222ed5274d7230a5baf9
Sasha Gypsy <oleksandrarevizina@tolik.training.local>
2014-08-12 14:29:38 +0300

    CoreData Manager - singleton

--- qwerty/BNRDetailViewController.m
+++ qwerty/BNRDetailViewController.m
@@ -457,8 +457,8 @@ static Route route;
     
     
     NSString *urlTemplate = nil;
-    urlTemplate = @"http://mt0.google.com/vt/x={x}&y={y}&z={z}";
-    //urlTemplate=@"http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png";
+    //urlTemplate = @"http://mt0.google.com/vt/x={x}&y={y}&z={z}";
+    urlTemplate=@"http://otile1.mqcdn.com/tiles/1.0.0/map/{z}/{x}/{y}.png";
     //http://static-maps.yandex.ru/1.x/?ll={x},{y}&size=450,450&z={z}&l=map
     //urlTemplate=@"http://static-maps.yandex.ru/1.x/?ll={x},{y}&size=256,256&z={z}&l=map";
     self.tileOverlay = [[MapTileOverlay alloc] initWithURLTemplate:urlTemplate];
